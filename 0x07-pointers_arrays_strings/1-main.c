64ebe4798a7615fff3b4c77c4628463bd3874560
mathengejob <jobmathenge90@gmail.com>
2022-03-29 13:38:00 -0700

    function that copies memory area.

--- 0x07-pointers_arrays_strings/1-main.c
+++ 0x07-pointers_arrays_strings/1-main.c
@@ -39,7 +39,7 @@ int main(void)
     char buffer[98] = {0};
     char buffer2[98] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
 
-    simple_print_buffer(buffer, 98);
+     simple_print_buffer(buffer, 98);
     _memcpy(buffer + 50, buffer2, 10);
     printf("-------------------------------------------------\n");
     simple_print_buffer(buffer, 98);    
