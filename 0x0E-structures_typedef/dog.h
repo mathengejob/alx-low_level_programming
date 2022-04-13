6ee53ba682fca28a6b2d562fe87deb9ec217314b
mathengejob <jobmathenge90@gmail.com>
2022-04-12 18:41:04 -0700

    a new type struct dog with the elements name age and owner

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef FILE_DOG
+#define FILE_DOG
+
+/**
+ * struct dog - structure dog
+ * @name: atribute pointer to char name's dog
+ * @age: atribute float age's dog
+ * @owner: atribute pointer to char owner's dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
