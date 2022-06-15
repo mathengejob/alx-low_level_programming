bd9fd96d51f936f29fa6b67f7b3bd515ad20dab9
mathengejob <jobmathenge90@gmail.com>
2022-06-14 19:17:19 -0700

    Correction on Dynamic lib

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,10 +1,7 @@
-#ifdef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <stlib.h>
-
-void _puts(char *s);
+#include "stdlib.h" 
 int _putchar(char c);
 int _putchar(char c);
 int _islower(int c);
@@ -13,7 +10,7 @@ int _abs(int n);
 int _isupper(int c);
 int _isdigit(int c);
 int _strlen(char *s);
-//void _puts(char *s);
+void _puts(char *s);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strcat(char *dest, char *src);
